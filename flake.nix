bf6869d05c8b02d5cc788b75303331376675f932
btwotwo <10519967+btwotwo@users.noreply.github.com>
2025-05-18 21:16:19 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,26 +1,60 @@
 {
-  description = "Rust flake";
-  inputs =
-    {
-      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  description = "A Nix-flake-based Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-  
-  outputs = { self, nixpkgs, ... }@inputs:
+  };
+
+  outputs = inputs:
     let
-     system = "x86_64-linux";
-     pkgs = nixpkgs.legacyPackages.${system};    
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: inputs.nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            inputs.rust-overlay.overlays.default
+            inputs.self.overlays.default
+          ];
+        };
+      });
     in
     {
-      devShells.${system}.default = pkgs.mkShell
-      {
-        packages = [ pkgs.rustc
-                     pkgs.cargo
-                     pkgs.rust-analyzer
-                     pkgs.rustfmt
-                     (pkgs.diesel-cli.override {
-                       sqliteSupport = true;
-                     })
-                   ];
+      overlays.default = final: prev: {
+        rustToolchain =
+          let
+            rust = prev.rust-bin;
+          in
+          if builtins.pathExists ./rust-toolchain.toml then
+            rust.fromRustupToolchainFile ./rust-toolchain.toml
+          else if builtins.pathExists ./rust-toolchain then
+            rust.fromRustupToolchainFile ./rust-toolchain
+          else
+            rust.stable.latest.default.override {
+              extensions = [ "rust-src" "rustfmt" ];
+            };
       };
+
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            rustToolchain
+            openssl
+            pkg-config
+            cargo-deny
+            cargo-edit
+            cargo-watch
+            rust-analyzer
+          ];
+
+          env = {
+            # Required by rust-analyzer
+            RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+          };
+        };
+      });
     };
 }
