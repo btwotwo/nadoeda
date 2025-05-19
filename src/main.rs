fb1c870ada7e8ad3bd459d49722c3d2d00bb1889
btwotwo <10519967+btwotwo@users.noreply.github.com>
2025-05-19 20:51:19 +0200

    Refactors

--- src/main.rs
+++ src/main.rs
@@ -39,23 +39,24 @@ impl ReminderWorker for PrinterWorker {
     }
 }
 
-struct ReminderScheduler {
-    tasks: HashMap<u64, ScheduledTask>,
-    cancellation_token: CancellationToken,
+
+struct ReminderManager {
+    tasks: HashMap<i32, ScheduledTask>
 }
 
+
+struct ReminderScheduler {}
+
 impl ReminderScheduler {
-    pub fn schedule_reminder<TWorker: ReminderWorker + Send + Sync>(
+    pub fn schedule_reminder<TWorker: ReminderWorker + Send + Sync + 'static>(
         &mut self,
         reminder: Reminder,
-        worker: &'static TWorker,
+        worker: TWorker,
     ) {
-        let cancellation_token = self.cancellation_token.child_token();
-        let task_cancellation_token = cancellation_token.clone();
+        let cancellation_token = CancellationToken::new();
+        let task_cancellation_token = cancellation_token.child_token();
 
         let reminder_id = reminder.id;
-
-        let previous_task = self.tasks.remove(&reminder.id);
 
         let now = Utc::now();
         let delay = Self::get_target_delay(&reminder, now)
@@ -63,22 +64,13 @@ impl ReminderScheduler {
             .expect("The target delay is always in the future.");
 
         let task_handle = tokio::spawn(async move {
-            Self::do_work(
-                previous_task,
-                task_cancellation_token,
-                reminder,
-                delay,
-                worker,
-            )
-            .await
+            Self::do_work(task_cancellation_token, reminder, delay, worker).await
         });
 
-        let scheduled_task = ScheduledTask {
+        ScheduledTask {
             task_handle,
             cancellation_token,
         };
-
-        self.tasks.insert(reminder_id, scheduled_task);
     }
 
     fn get_target_delay(reminder: &Reminder, now: DateTime<Utc>) -> Duration {
@@ -86,16 +78,11 @@ impl ReminderScheduler {
     }
 
     async fn do_work<TWorker: ReminderWorker + Send + Sync>(
-        previous_task: Option<ScheduledTask>,
         cancellation_token: CancellationToken,
         reminder: Reminder,
         delay: std::time::Duration,
-        worker: &TWorker,
+        worker: TWorker,
     ) {
-        if let Some(previous_task) = previous_task {
-            Self::cancel_existing(previous_task).await;
-        }
-
         tokio::select! {
             _ = cancellation_token.cancelled() => {
                 println!("Task for scheduling reminder was cancelled. {:?}", reminder)
@@ -104,14 +91,6 @@ impl ReminderScheduler {
                 worker.handle_reminder(reminder).await;
             }
         }
-    }
-
-    async fn cancel_existing(schedule_task: ScheduledTask) {
-        schedule_task.cancellation_token.cancel();
-        schedule_task
-            .task_handle
-            .await
-            .unwrap_or_else(|e| println!("Error while cancelling task. {:?}", e))
     }
 }
 
@@ -124,42 +103,12 @@ async fn main() {
 mod tests {
     use crate::*;
     use chrono::NaiveTime;
+    use std::{
+        borrow::BorrowMut,
+        cell::{Cell, RefCell},
+        collections::HashMap,
+        time::Duration,
+    };
     use tokio::time;
-    use std::{borrow::BorrowMut, cell::{Cell, RefCell}, collections::HashMap, time::Duration};
     use tokio_util::sync::CancellationToken;
-
-    /// SAFETY: Used only in unit tests, is not shared between threads.
-    unsafe impl Sync for MockWorker {}
-    struct MockWorker {
-        received_reminders: RefCell<Vec<Reminder>>
-    }
-    
-    impl ReminderWorker for MockWorker {
-        async fn handle_reminder(&self, reminder: Reminder) {
-            let mut a = self.received_reminders.borrow_mut();
-            a.push(reminder);
-        }
-    }
-
-    #[tokio::test(start_paused = true)]
-    async fn given_scheduler_when_reminder_scheduled_should_be_processed_after_timeout() {
-        let mut scheduler = ReminderScheduler {
-            cancellation_token: CancellationToken::new(),
-            tasks: HashMap::new(),
-        };
-
-        let reminder = Reminder {
-            id: 0,
-            state: ReminderState::Scheduled,
-            fire_at: NaiveTime::from_hms_opt(12, 0, 0).unwrap(),
-        };
-        
-        let worker = Box::leak(Box::new(MockWorker {
-            received_reminders: RefCell::new(Vec::new())
-        }));
-
-        scheduler.schedule_reminder(reminder, worker);
-        time::sleep(Duration::from_secs(11)).await;
-        assert!(worker.received_reminders.borrow().len() == 1)
-    }
 }
