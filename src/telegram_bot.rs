b19add8d51690f594a8b5e1eec39cc4f63567592
btwotwo <10519967+btwotwo@users.noreply.github.com>
2025-06-07 22:16:42 +0200

    Add handlers for reminder text and time

--- src/telegram_bot.rs
+++ src/telegram_bot.rs
@@ -1,3 +1,4 @@
+use chrono::NaiveTime;
 use dptree::case;
 use teloxide::{
     dispatching::dialogue, dispatching::dialogue::InMemStorage, macros::BotCommands, prelude::*,
@@ -47,21 +48,35 @@ impl TelegramInteractionInterface {
         let bot = Bot::new(appsettings::get().telegram.token.clone());
         log::info!("Creating Telegram interaction interface");
 
-        let command_handler = teloxide::filter_command::<Command, _>().branch(
-            case![CreateReminderDialogueState::Start]
-                .branch(case![Command::CreateReminder].endpoint(Self::create_reminder_start)),
-        );
+        let command_handler = teloxide::filter_command::<Command, _>()
+            .branch(
+                case![CreateReminderDialogueState::Start]
+                    .branch(case![Command::CreateReminder].endpoint(Self::create_reminder_start)),
+                
+            )
+            .branch(case![Command::Cancel].endpoint(Self::cancel));
 
         let message_handler = Update::filter_message()
             .branch(command_handler)
-            .branch(case![CreateReminderDialogueState::ReceiveText].endpoint(Self::receive_reminder_text))
+            .branch(
+                case![CreateReminderDialogueState::ReceiveText]
+                    .endpoint(Self::receive_reminder_text),
+            )
+            .branch(case![CreateReminderDialogueState::ReceiveFiringTime {text}].endpoint(Self::receive_firing_time))
             .branch(dptree::endpoint(Self::invalid_state));
 
-        let schema  = dialogue::enter::<Update, InMemStorage<CreateReminderDialogueState>, CreateReminderDialogueState, _>()
-            .branch(message_handler);
+        let schema = dialogue::enter::<
+            Update,
+            InMemStorage<CreateReminderDialogueState>,
+            CreateReminderDialogueState,
+            _,
+        >()
+        .branch(message_handler);
 
         Dispatcher::builder(bot, schema)
-            .dependencies(dptree::deps![InMemStorage::<CreateReminderDialogueState>::new()])
+            .dependencies(dptree::deps![
+                InMemStorage::<CreateReminderDialogueState>::new()
+            ])
             .enable_ctrlc_handler()
             .build()
             .dispatch()
@@ -73,32 +88,76 @@ impl TelegramInteractionInterface {
         dialogue: ReminderCreationDialogue,
         msg: Message,
     ) -> HandlerResult {
-        bot.send_message(msg.chat.id, "Creating a new reminder! Please input reminder text").await?;
-        dialogue.update(CreateReminderDialogueState::ReceiveText).await?;
-        
+        bot.send_message(
+            msg.chat.id,
+            "Creating a new reminder! Please input reminder text. If you want to cancel, use the /cancel command.",
+        )
+        .await?;
+        dialogue
+            .update(CreateReminderDialogueState::ReceiveText)
+            .await?;
+
         Ok(())
     }
 
     async fn receive_reminder_text(
         bot: Bot,
         dialogue: ReminderCreationDialogue,
-        msg: Message
+        msg: Message,
     ) -> HandlerResult {
         match msg.text() {
             Some(text) => {
-                bot.send_message(msg.chat.id, format!("Great! Your reminder will say \"{}\"", text)).await?;
-                dialogue.update(CreateReminderDialogueState::ReceiveFiringTime { text: text.to_string() }).await?;
-            },
+                bot.send_message(
+                    msg.chat.id,
+                    format!("Great! Your reminder will say \"{}\"", text),
+                )
+                .await?;
+                dialogue
+                    .update(CreateReminderDialogueState::ReceiveFiringTime {
+                        text: text.to_string(),
+                    })
+                    .await?;
+            }
             None => {
-                bot.send_message(msg.chat.id, "Please send me reminder text.").await?;
+                bot.send_message(msg.chat.id, "Please send me reminder text.")
+                    .await?;
             }
         }
 
         Ok(())
     }
 
-    async fn invalid_state(bot: Bot, dialogue: ReminderCreationDialogue, msg: Message ) -> HandlerResult {
-        bot.send_message(msg.chat.id, "Unable to handle the message.").await?;
+    async fn receive_firing_time(
+        bot: Bot,
+        dialogue: ReminderCreationDialogue,
+        text: String,
+        msg: Message
+    ) -> HandlerResult {
+        match msg.text().map(|text| NaiveTime::parse_from_str(text, "%H:%M")) {
+            Some(Ok(time)) => {
+                bot.send_message(msg.chat.id, format!("Great! Your reminder will fire at {}", time)).await?;
+            }
+            _ => {
+                bot.send_message(msg.chat.id, "Could not parse time. Please send time in the following format: \"13:00\"").await?;
+            }
+        }
+        Ok(())
+    }
+
+    async fn invalid_state(
+        bot: Bot,
+        dialogue: ReminderCreationDialogue,
+        msg: Message,
+    ) -> HandlerResult {
+        bot.send_message(msg.chat.id, "Unable to handle the message.")
+            .await?;
+        Ok(())
+    }
+
+    async fn cancel(bot: Bot, dialogue: ReminderCreationDialogue, msg: Message) -> HandlerResult {
+        bot.send_message(msg.chat.id, "Cancelled current operation.")
+            .await?;
+        dialogue.exit().await?;
         Ok(())
     }
 }
