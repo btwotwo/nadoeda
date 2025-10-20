411fa3a2a00a0ed08cf094a5dcad2ee88d4f3c79
jocoding <jocoding@noreply.codeberg.org>
2025-10-20 16:16:37 +0200

    Set database location to tmpdir

--- flake.nix
+++ flake.nix
@@ -55,7 +55,7 @@
             # Required by rust-analyzer
             RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
             RUST_LOG = "trace";
-            DATABASE_URL = "sqlite://nadoeda.db";
+            DATABASE_URL = "sqlite:///tmp/nadoeda.db";
           };
         };
       });
