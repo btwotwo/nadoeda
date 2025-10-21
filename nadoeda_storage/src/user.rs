17bf321171ea9042f436d7ce25c7afe0cd0fa267
btwotwo <jocoding@tuta.com>
2025-10-21 15:12:22 +0200

    Finish user CRUD operations

--- nadoeda_storage/src/user.rs
+++ nadoeda_storage/src/user.rs
@@ -20,14 +20,17 @@ pub trait UserInfoStorage {
 
     async fn get(&self, id: UserId) -> Result<Option<User>, Self::Error>;
     async fn get_by_tg_chat(&self, chat_id: i64) -> Result<Option<User>, Self::Error>;
-    async fn create(&self, new_user: NewUser) -> Result<UserId, Self::Error>;
-    async fn update(&self, update_user: UpdateUser) -> Result<UserId, Self::Error>;
+    async fn create(&self, new_user: NewUser) -> Result<User, Self::Error>;
+    async fn update(&self, update_user: User) -> Result<User, Self::Error>;
     async fn delete(&self, id: UserId) -> Result<(), Self::Error>;
 }
 
 mod sqlite_user_storage {
     use async_trait::async_trait;
-    use nadoeda_models::{chrono_tz::{self, Tz}, user::{User, UserId}};
+    use nadoeda_models::{
+        chrono_tz::{self, Tz},
+        user::{User, UserId},
+    };
 
     use super::{NewUser, UpdateUser, UserInfoStorage};
 
@@ -53,7 +56,7 @@ mod sqlite_user_storage {
             Self {
                 id: value.id,
                 tg_chat_id: value.tg_chat_id,
-                timezone: value.timezone.parse().unwrap_or_default()
+                timezone: value.timezone.parse().unwrap_or_default(),
             }
         }
     }
@@ -73,36 +76,71 @@ mod sqlite_user_storage {
         type Error = anyhow::Error;
 
         async fn get(&self, id: UserId) -> Result<Option<User>, Self::Error> {
-            let user = sqlx::query_as!(UserStorageModel, "SELECT * FROM users WHERE id = ?;", id).fetch_optional(&self.pool).await?;
+            let user = sqlx::query_as!(UserStorageModel, "SELECT * FROM users WHERE id = ?;", id)
+                .fetch_optional(&self.pool)
+                .await?;
 
             Ok(user.map(Into::into))
         }
         async fn get_by_tg_chat(&self, chat_id: i64) -> Result<Option<User>, Self::Error> {
-            let user = sqlx::query_as!(UserStorageModel,
+            let user = sqlx::query_as!(
+                UserStorageModel,
                 "SELECT * FROM users WHERE tg_chat_id = ?",
                 chat_id
-            ).fetch_optional(&self.pool).await?;
+            )
+            .fetch_optional(&self.pool)
+            .await?;
 
             Ok(user.map(Into::into))
         }
-        async fn create(&self, new_user: NewUser) -> Result<UserId, Self::Error> {
-            let NewUser {tg_chat_id, timezone} = new_user;
+        async fn create(&self, new_user: NewUser) -> Result<User, Self::Error> {
+            let NewUser {
+                tg_chat_id,
+                timezone,
+            } = new_user;
             let timezone = timezone.to_string();
-            let user_id = sqlx::query_scalar!(
+            let user = sqlx::query_as!(
+                UserStorageModel,
                 "INSERT INTO users (tg_chat_id, timezone)
                  VALUES (?, ?)
-                 RETURNING id",
+                 RETURNING *",
                 tg_chat_id,
                 timezone
-            ).fetch_one(&self.pool).await?;
+            )
+            .fetch_one(&self.pool)
+            .await?;
 
-            Ok(user_id)
+            Ok(user.into())
         }
-        async fn update(&self, update_user: UpdateUser) -> Result<UserId, Self::Error> {
-            todo!()
+        async fn update(&self, update_user: User) -> Result<User, Self::Error> {
+            let User {
+                id,
+                tg_chat_id,
+                timezone,
+            } = update_user;
+            let timezone = timezone.to_string();
+            let user = sqlx::query_as!(
+                UserStorageModel,
+                "UPDATE users
+                 SET tg_chat_id = ?,
+                     timezone = ?
+                 WHERE id = ?
+                 RETURNING *",
+                tg_chat_id,
+                timezone,
+                id
+            )
+            .fetch_one(&self.pool)
+            .await?;
+
+            Ok(user.into())
         }
         async fn delete(&self, id: UserId) -> Result<(), Self::Error> {
-            todo!()
+            sqlx::query_as!(UserStorageModel, "DELETE FROM users WHERE id = ?", id)
+                .execute(&self.pool)
+                .await?;
+
+            Ok(())
         }
     }
 }
