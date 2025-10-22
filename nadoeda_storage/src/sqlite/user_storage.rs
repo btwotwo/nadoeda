e28f0a648147300b030720ced9e2edb47a2860da
btwotwo <jocoding@tuta.com>
2025-10-22 21:53:50 +0200

    Move user storage model to separate module

--- nadoeda_storage/src/sqlite/user_storage.rs
+++ nadoeda_storage/src/sqlite/user_storage.rs
@@ -1,34 +1,10 @@
+mod model;
+
 use async_trait::async_trait;
+use model::UserStorageModel;
 use nadoeda_models::user::{User, UserId};
 
 use crate::user::{NewUser, UserInfoStorage};
-
-#[derive(sqlx::FromRow, sqlx::Type)]
-struct UserStorageModel {
-    pub id: i64,
-    pub timezone: String,
-    pub tg_chat_id: Option<i64>,
-}
-
-impl From<User> for UserStorageModel {
-    fn from(value: User) -> Self {
-        Self {
-            id: value.id,
-            timezone: value.timezone.to_string(),
-            tg_chat_id: value.tg_chat_id,
-        }
-    }
-}
-
-impl From<UserStorageModel> for User {
-    fn from(value: UserStorageModel) -> Self {
-        Self {
-            id: value.id,
-            tg_chat_id: value.tg_chat_id,
-            timezone: value.timezone.parse().unwrap_or_default(),
-        }
-    }
-}
 
 pub struct SqliteUserInfoStorage {
     pool: sqlx::SqlitePool,
