a16bfd95bbef4fb857955d3e3d20a79e43bb6e48
jocoding <jocoding@noreply.codeberg.org>
2025-10-22 17:16:03 +0200

    Add all CRUD operations for reminder storage

--- nadoeda_storage/src/sqlite/reminder_storage.rs
+++ nadoeda_storage/src/sqlite/reminder_storage.rs
@@ -1,10 +1,12 @@
 mod model;
 
+use crate::{ReminderStorage, reminder::NewReminder};
 use async_trait::async_trait;
-use nadoeda_models::{reminder::{Reminder, ReminderId}, user::UserId};
-use model::ReminderStorageModel;
-use crate::{reminder::NewReminder, ReminderStorage};
-
+use model::{ReminderStorageModel, convert_state};
+use nadoeda_models::{
+    reminder::{Reminder, ReminderId, ReminderState},
+    user::UserId,
+};
 
 pub struct SqliteReminderStorage {
     pool: sqlx::SqlitePool,
@@ -13,19 +15,82 @@ pub struct SqliteReminderStorage {
 #[async_trait]
 impl ReminderStorage for SqliteReminderStorage {
     type Error = anyhow::Error;
-    
+
     async fn get(&self, id: ReminderId) -> Result<Option<Reminder>, Self::Error> {
-        let reminder = sqlx::query_as!(ReminderStorageModel,
-            "SELECT * FROM reminders WHERE id = ?", id).fetch_optional(&self.pool).await?;
-        todo!()
+        let reminder = sqlx::query_as!(
+            ReminderStorageModel,
+            "SELECT * FROM reminders WHERE id = ?",
+            id
+        )
+        .fetch_optional(&self.pool)
+        .await?;
+
+        Ok(reminder.map(Into::into))
     }
     async fn get_all_user_reminders(&self, user_id: UserId) -> Result<Vec<Reminder>, Self::Error> {
-        todo!()
+        let reminders = sqlx::query_as!(
+            ReminderStorageModel,
+            "SELECT * FROM reminders WHERE user_id = ?",
+            user_id
+        )
+        .fetch_all(&self.pool)
+        .await?;
+
+        Ok(reminders.into_iter().map(Into::into).collect())
     }
     async fn insert(&self, reminder: NewReminder) -> Result<Reminder, Self::Error> {
-        todo!()
+        let NewReminder {
+            text,
+            fire_at,
+            user_id,
+        } = reminder;
+        let (state_kind, attempts_left) = convert_state(ReminderState::Pending);
+        let fire_at = fire_at.into_string();
+
+        let created_reminder = sqlx::query_as!(
+            ReminderStorageModel,
+            "INSERT INTO reminders (user_id, state_kind, attempts_left, fire_at, text)
+VALUES (?, ?, ?, ?, ?) RETURNING *",
+            user_id,
+            state_kind,
+            attempts_left,
+            fire_at,
+            text
+        )
+        .fetch_one(&self.pool)
+        .await?;
+
+        Ok(created_reminder.into())
     }
+
     async fn update(&self, reminder: Reminder) -> Result<Reminder, Self::Error> {
-        todo!()
+        let ReminderStorageModel {
+            id,
+            user_id,
+            state_kind,
+            attempts_left,
+            fire_at,
+            text,
+        } = reminder.into();
+        let updated_reminder = sqlx::query_as!(
+            ReminderStorageModel,
+            "
+UPDATE reminders
+SET state_kind = ?,
+    attempts_left = ?,
+    fire_at = ?,
+    text = ?
+WHERE id = ?
+RETURNING *
+",
+            state_kind,
+            attempts_left,
+            fire_at,
+            text,
+            id
+        )
+        .fetch_one(&self.pool).await?;
+
+        Ok(updated_reminder.into())
     }
 }
