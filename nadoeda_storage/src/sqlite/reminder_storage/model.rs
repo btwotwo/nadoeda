0f327c73ec454513545d22fe80b0b0b7fc516b00
jocoding <jocoding@noreply.codeberg.org>
2025-10-22 17:18:03 +0200

    Use domain methods

--- nadoeda_storage/src/sqlite/reminder_storage/model.rs
+++ nadoeda_storage/src/sqlite/reminder_storage/model.rs
@@ -4,13 +4,12 @@ use nadoeda_models::{
     user::UserId,
 };
 
-#[derive(sqlx::FromRow)]
 pub struct ReminderStorageModel {
     pub id: i64,
     pub user_id: i64,
     pub state_kind: String,
-    pub attempts_left: Option<u8>,
-    pub fire_at: sqlx::types::chrono::NaiveTime,
+    pub attempts_left: Option<i64>,
+    pub fire_at: String,
     pub text: String,
 }
 
@@ -21,7 +20,7 @@ impl From<Reminder> for ReminderStorageModel {
             id: value.id,
             user_id: value.user_id,
             text: value.text,
-            fire_at: value.fire_at.into_time(),
+            fire_at: value.fire_at.into_string(),
             state_kind: state,
             attempts_left,
         }
@@ -31,37 +30,37 @@ impl From<Reminder> for ReminderStorageModel {
 impl From<ReminderStorageModel> for Reminder {
     fn from(value: ReminderStorageModel) -> Self {
         let state = parse_state(&value.state_kind, value.attempts_left);
-
+        let fire_at = ReminderFireTime::from_string(&value.fire_at).unwrap();
         Self {
             id: value.id,
             user_id: value.user_id,
-            fire_at: ReminderFireTime::new(value.fire_at),
+            fire_at,
             text: value.text,
             state,
         }
     }
 }
 
-fn convert_state(state: ReminderState) -> (String, Option<u8>) {
+pub fn convert_state(state: ReminderState) -> (String, Option<i64>) {
     match state {
         ReminderState::Pending => ("Pending".to_string(), None),
         ReminderState::Scheduled => ("Scheduled".to_string(), None),
-        ReminderState::Nagging { attempts_left } => ("Nagging".to_string(), Some(attempts_left)),
+        ReminderState::Nagging { attempts_left } => ("Nagging".to_string(), Some(attempts_left as i64)),
         ReminderState::Confirming { attempts_left } => {
-            ("Confirming".to_string(), Some(attempts_left))
+            ("Confirming".to_string(), Some(attempts_left as i64))
         }
     }
 }
 
-fn parse_state(state: &str, attempts_left: Option<u8>) -> ReminderState {
+pub fn parse_state(state: &str, attempts_left: Option<i64>) -> ReminderState {
     match state {
         "Pending" => ReminderState::Pending,
         "Scheduled" => ReminderState::Scheduled,
         "Nagging" => ReminderState::Nagging {
-            attempts_left: attempts_left.unwrap_or(3),
+            attempts_left: attempts_left.unwrap_or(3) as u8,
         },
         "Confirming" => ReminderState::Confirming {
-            attempts_left: attempts_left.unwrap_or(3),
+            attempts_left: attempts_left.unwrap_or(3) as u8,
         },
         other => {
             log::warn!("Warning: Unknown state {}, defaulting to Pending", other);
