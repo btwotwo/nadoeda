a7128c611fe6b8b52fdd1016eac43f638548288f
jocoding <jocoding@noreply.codeberg.org>
2025-11-28 15:45:34 +0100

    Onboarding is ready

--- nadoeda_telegram/src/authenticate_user.rs
+++ nadoeda_telegram/src/authenticate_user.rs
@@ -73,7 +73,7 @@ pub async fn get_user_timezone(
                     })
                     .await?;
 
-                bot.send_message(msg.chat.id, "Timezone received. Welcome aboard.");
+                bot.send_message(msg.chat.id, "Timezone received. Welcome aboard.").await?;
 
                 dialogue
                     .update(GlobalState::AuthenticatedV2(
@@ -95,10 +95,12 @@ pub async fn get_user_timezone(
 }
 
 pub(super) fn schema() -> UpdateHandler<anyhow::Error> {
-    dptree::entry().branch(
-        Update::filter_message()
-            .branch(case![GlobalState::Unauthenticated].endpoint(try_authenticate)),
-    )
+    Update::filter_message()
+        .branch(case![GlobalState::Unauthenticated].endpoint(try_authenticate))
+        .branch(
+            case![GlobalState::Authenticating(x)]
+                .branch(case![AuthenticationState::WaitingForTimezone].endpoint(get_user_timezone)),
+        )
 }
 
 #[cfg(test)]
@@ -188,5 +190,39 @@ mod tests {
             crate::AuthenticatedActionState::Idle,
         ))
         .await
+    }
+
+    #[sqlx::test(migrations = "../nadoeda_storage/migrations")]
+    async fn given_provided_correct_timezone_should_set_authenticated_state(pool: Pool<Sqlite>) {
+        let storage = storage(pool.clone());
+        let user_storage = user_storage(pool.clone());
+        let schema =
+            dialogue::enter::<Update, InMemStorage<GlobalState>, GlobalState, _>().branch(schema());
+        let mock_message = MockMessageText::new().text("Europe/Prague");
+        let chat_id = mock_message.chat.id;
+
+        let mut bot = MockBot::new(mock_message, schema);
+
+        bot.dependencies(deps![
+            storage,
+            user_storage,
+            InMemStorage::<GlobalState>::new(),
+            GlobalState::Unauthenticated
+        ]);
+
+        bot.set_state(GlobalState::Authenticating(
+            AuthenticationState::WaitingForTimezone,
+        ))
+        .await;
+
+        bot.dispatch_and_check_state(GlobalState::AuthenticatedV2(
+            AuthenticationInfo(User {
+                id: 1,
+                timezone: chrono_tz::Tz::Europe__Prague,
+                tg_chat_id: Some(chat_id.0),
+            }),
+            AuthenticatedActionState::Idle,
+        ))
+        .await;
     }
 }
