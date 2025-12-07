c3b566f3b1f9daf09d60b23c7fc2b63ce334b339
btwotwo <jocoding@tuta.com>
2025-12-07 21:27:10 +0100

    Preparations for reminder editing

--- nadoeda_telegram/src/edit_reminders.rs
+++ nadoeda_telegram/src/edit_reminders.rs
@@ -2,12 +2,12 @@ use std::sync::Arc;
 
 use dptree::case;
 use nadoeda_storage::{ReminderStorage, sqlite::reminder_storage::SqliteReminderStorage};
-use teloxide::dispatching::UpdateHandler;
-use teloxide::prelude::*;
+use teloxide::{dispatching::UpdateHandler, macros::BotCommands};
+use teloxide::{filter_command, prelude::*};
 use teloxide::types::ParseMode;
 use teloxide::utils::markdown;
 
-use nadoeda_models::reminder::Reminder;
+use nadoeda_models::reminder::{Reminder, ReminderId};
 
 use crate::AuthenticatedActionState;
 
@@ -17,6 +17,17 @@ use super::{GlobalCommand, GlobalDialogue, HandlerResult};
 pub(super) enum EditRemindersState {
     #[default]
     Start,
+}
+
+
+#[derive(BotCommands, Clone)]
+#[command(
+    rename_rule = "lowercase",
+    parse_with = "split",
+    command_separator = "_"
+)]
+enum EditReminderCommand {
+    Edit(ReminderId)
 }
 
 async fn list_reminders(
@@ -44,6 +55,11 @@ async fn list_reminders(
     Ok(())
 }
 
+async fn edit_reminder(id: ReminderId, msg: Message, bot: Bot) -> HandlerResult {
+    bot.send_message(msg.chat.id, format!("Editing reminder {id}")).await?;
+    Ok(())
+}
+
 fn display_reminder(order: usize, reminder: &Reminder) -> String {
     format!(
         "{order}: *{0}* \\(remind every day at *{1}*\\)
@@ -57,9 +73,14 @@ Edit \\- /edit\\_{2}",
 pub(super) fn schema() -> UpdateHandler<anyhow::Error> {
     dptree::entry().branch(
         case![AuthenticatedActionState::Idle].branch(
-            Update::filter_message()
-                .filter_command::<GlobalCommand>()
-                .branch(case![GlobalCommand::ListReminders].endpoint(list_reminders)),
+            Update::filter_message().branch(
+                filter_command::<GlobalCommand, _>()
+                    .branch(case![GlobalCommand::ListReminders].endpoint(list_reminders)),
+            ).branch(
+                filter_command::<EditReminderCommand, _>().branch(
+                    case![EditReminderCommand::Edit(id)].endpoint(edit_reminder)
+                )
+            ),
         ),
     )
 }
